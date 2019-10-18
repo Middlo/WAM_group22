7ee4812af21b984892d8f989fb89640e1534d480
Eyuell <eyuell@gmail.com>
2019-10-18 08:12:19 +0200

    Update MainActivity.java

--- Android/app/src/main/java/se/chalmers/cse/dit341/group22/MainActivity.java
+++ Android/app/src/main/java/se/chalmers/cse/dit341/group22/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
         //Init View
 
